93716b91ab63bd291c44436a8c8185728afdcbf8
Code Barru <antoine.ousselin@protonmail.com>
2025-06-15 14:19:31 +0200

    feat: agent auto reconnect

--- agent/src/main.rs
+++ agent/src/main.rs
@@ -1,22 +1,47 @@
 mod network;
 use network::Client;
 
-fn main() -> std::io::Result<()> {
-    let mut client = Client::new("127.0.0.1:1337")?;
-
-    println!("Connected to server successfully!");
-
+fn main() {
     loop {
-        client.send(b"Hello, server!")?;
-        let response = client.receive()?;
-        if response.is_empty() {
-            println!("Server closed the connection.");
-            break;
-        }
-        println!("Received from server: {:?}", response);
-        std::thread::sleep(std::time::Duration::from_secs(5));
-    }
+        let mut client = loop {
+            match Client::new("127.0.0.1:1337") {
+                Ok(client) => break client,
+                Err(_) => {
+                    eprintln!("Failed to connect to server, retrying in 5 seconds..");
+                    std::thread::sleep(std::time::Duration::from_secs(5));
+                }
+            }
+        };
+        println!("Connected to server successfully!");
 
-    client.shutdown()?;
-    Ok(())
+        loop {
+            let res = client.send(b"Hello from client!");
+            if let Err(_) = res {
+                eprintln!("Failed to send message, server may be down. Reconnecting in 5s...");
+                break;
+            }
+
+            let response = match client.receive() {
+                Ok(response) => response,
+                Err(_) => {
+                    eprintln!(
+                        "Failed to receive response, server may be down. Reconnecting in 5s..."
+                    );
+                    break;
+                }
+            };
+
+            if response.is_empty() {
+                println!("Server closed the connection.");
+                break;
+            }
+            println!("Received from server: {:?}", response);
+            std::thread::sleep(std::time::Duration::from_secs(5));
+        }
+
+        match client.shutdown() {
+            Ok(_) => (),
+            Err(e) => eprintln!("Failed to close connection: {}", e),
+        };
+    }
 }
