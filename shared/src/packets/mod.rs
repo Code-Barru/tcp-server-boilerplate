3ae4adf3aafaaa4694111b9c918508208d9f90e4
Code-Barru <antoine.ousselin@protonmail.com>
2025-12-07 20:52:44 +0100

    refacto: moved encryption packets to one module

--- shared/src/packets/mod.rs
+++ shared/src/packets/mod.rs
@@ -1,11 +1,9 @@
-mod encryption_request;
-mod encryption_response;
+mod encryption;
 mod heartbeat;
 mod packet;
 mod stream;
 
-pub use encryption_request::EncryptionRequest;
-pub use encryption_response::EncryptionResponse;
+pub use encryption::{EncryptionRequest, EncryptionResponse};
 pub use heartbeat::Heartbeat;
 pub use packet::{PacketError, Packet, Packets, from_packet_bytes};
 pub use stream::{StreamOpen, StreamClose, StreamData, StreamError};
